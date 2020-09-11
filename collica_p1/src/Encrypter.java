ab619ac82d1dcf66f31d4177eb75c0c837425bb7
Giovanni Collica <giovannijcollica@gmail.com>
2020-09-11 16:27:48 -0400

    Add comments to encrypter class

--- collica_p1/src/Encrypter.java
+++ collica_p1/src/Encrypter.java
@@ -1,9 +1,22 @@
+/*
+    Giovanni Collica
+    Programming Assignment 1
+    COP 3330
+
+    This java class is designed to take in
+    a string of numbers and encrypt them
+    according to a specific algorithm set
+    out by the assignment.
+ */
+
+//main encrypter class
 public class Encrypter {
     private String originalNumString;
     private String encryptedNumString;
     int [] originalNumArray;
     int [] encryptedNumArray;
 
+    //main function to encrypt the string
     public String encrypt(String number){
         this.originalNumString = number;
 
@@ -17,15 +30,18 @@ public class Encrypter {
 
     }
 
+    //function to convert the integer array into a string
     public String convertToString(int[] encryptedNumbers){
         String Number = "";
         int size = encryptedNumbers.length;
         char[] numbers = new char[encryptedNumbers.length];
 
+        //convert integer array into char array
         for(int i = 0; i < size; i++){
             numbers[i] = Character.forDigit((encryptedNumbers[i]), 10);
         }
 
+        //convert char array into string
         for(int j = 0; j < size; j++){
             Number += String.valueOf(numbers[j]);
         }
@@ -33,6 +49,7 @@ public class Encrypter {
         return Number;
     }
 
+    //function to convert the string into an integer array
     public int[] convertToArray(String numberToEncrypt){
         char[] numbers = new char[numberToEncrypt.length()];
         int size = numberToEncrypt.length();
@@ -49,6 +66,7 @@ public class Encrypter {
         return numArray;
     }
 
+    //function to add seven to each number in the integer array
     public int[] addSeven(int[] numberToEncrypt){
         int size = numberToEncrypt.length;
         for(int i = 0; i < size; i++){
@@ -57,6 +75,7 @@ public class Encrypter {
         return numberToEncrypt;
     }
 
+    //function to get the remainder for each number in the integer array
     public int[] getRemainder(int[] numberToEncrypt){
         int size = numberToEncrypt.length;
         for(int i = 0; i < size; i++){
@@ -65,6 +84,8 @@ public class Encrypter {
         return numberToEncrypt;
     }
 
+    //function to swap the first and third digits and
+    //the second and fourth digits
     public int[] swapDigits(int[] numberToEncrypt){
         int temp = 0;
 
