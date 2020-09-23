367c7f5a19e71bcefa608b60ff29bbda8f7af844
Giovanni Collica <giovannijcollica@gmail.com>
2020-09-23 16:50:43 -0400

    Rework empty methods and fill in moreInput method

--- collica_p2/src/App.java
+++ collica_p2/src/App.java
@@ -18,25 +18,36 @@ public class App {
         displayBmiStatistics(bmiData);
     }
 
+    private static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData) {
+    }
+
+    private static void displayBmiInfo(BodyMassIndex bmi) {
+    }
+
+    private static double getUserHeight() {
+    }
+
+    private static double getUserWeight() {
+
+    }
+
+
     public static boolean moreInput(){
-        System.out.println("Do you want to calculate another BMi?");
-        Scanner yesOrNo = S
+        System.out.println("Do you want to calculate another BMI?");
+        System.out.println("Enter Y for yes, or N for no:");
+        Scanner input = new Scanner(System.in);
+        input.nextLine();
+        String yesOrNo = input.nextLine();
+        if(yesOrNo.equals("Y")) {
+            return true;
+        } else if(yesOrNo.equals("N")) {
+            return false;
+        } else {
+            System.out.println("Input not recognized");
+            return false;
+        }
     }
 
-    public double getUserHeight(){
 
-    }
-
-    public double getUserHeight(){
-
-    }
-
-    public static void displayBmiInfo(){
-
-    }
-
-    public static void displayBmiStatistics(){
-
-    }
 
 }
