b8e5626754299755e925823e9f6d61bc80314619
Giovanni Collica <giovannijcollica@gmail.com>
2020-09-28 00:54:19 -0400

    Intialize BodyMassIndexTest class

--- collica_p2/src/BodyMassIndexTest.java
+++ collica_p2/src/BodyMassIndexTest.java
@@ -1,2 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BodyMassIndexTest {
+
+    @org.junit.jupiter.api.Test
+    void calculateBmiNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateBmiCategory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBmi() {
+    }
+
+    @Test
+
+
+
+
+
 }
