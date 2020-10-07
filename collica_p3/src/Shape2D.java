52e4a3233aa6414aac535ad3eac5c9a0988d1028
Giovanni Collica <giovannijcollica@gmail.com>
2020-10-07 16:06:55 -0400

    Begin getArea method

--- collica_p3/src/Shape2D.java
+++ collica_p3/src/Shape2D.java
@@ -1,2 +1,28 @@
 public class Shape2D extends Shape {
+    public double getArea(){
+        String shape = getName();
+        if(shape == "square"){
+            double area;
+            double sideLength = getSideLength();
+            area = sideLength * sideLength;
+
+            return area;
+        } else if(shape == "triangle"){
+            double area;
+            double base = getBase();
+            double height = getHeight();
+            area = (1/2) * (base * height);
+
+            return area;
+        } else if(shape == "circle"){
+            double area;
+            double radius = getRadius();
+            area = Math.PI * (radius * radius);
+
+            return area;
+        } else {
+            return 0;
+        }
+    }
+
 }
