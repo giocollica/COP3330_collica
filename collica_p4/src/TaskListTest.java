2b1024f2745629889fe0bb654f859e5f1ea940eb
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-10 12:25:31 -0500

    Add in prototypes for tests

--- collica_p4/src/TaskListTest.java
+++ collica_p4/src/TaskListTest.java
@@ -1,2 +1,115 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskListTest {
+    @Test
+    addingTaskItemsIncreasesSize(){
+
+    }
+
+    @Test
+    completingTaskItemChangesStatus(){
+
+    }
+
+    @Test
+    completingTaskItemFailsWithInvalidIndex(){
+
+    }
+
+    @Test
+    editingTaskItemChangesValues(){
+
+    }
+
+    @Test
+    editingTaskItemDescriptionChangesValue(){
+
+    }
+
+    @Test
+    editingTaskItemDescriptionFailsWithInvalidIndex(){
+
+    }
+
+    @Test
+    editingTaskItemDueDateChangesValue(){
+
+    }
+
+    @Test
+    editingTaskItemDueDateFailsWithInvalidIndex(){
+
+    }
+
+    @Test
+    editingTaskItemTitleChangesValue(){
+
+    }
+
+    @Test
+    editingTaskItemTitleFailsWithInvalidIndex(){
+
+    }
+
+    @Test
+    gettingTaskItemDescriptionFailsWithInvalidIndex(){
+
+    }
+
+    @Test
+    gettingTaskItemDescriptionSucceedsWithValidIndex(){
+
+    }
+
+    @Test
+    gettingTaskItemDueDateFailsWithInvalidIndex(){
+
+    }
+
+    @Test
+    gettingTaskItemDueDateSucceedsWithValidIndex(){
+
+    }
+
+    @Test
+    gettingTaskItemTitleFailsWithInvalidIndex(){
+
+    }
+
+    @Test
+    gettingTaskItemTitleSucceedsWithValidIndex(){
+
+    }
+
+    @Test
+    newTaskListIsEmpty(){
+
+    }
+
+    @Test
+    removingTaskItemsDecreasesSize(){
+
+    }
+
+    @Test
+    removingTaskItemsFailsWithInvalidIndex(){
+
+    }
+
+    @Test
+    savedTaskListCanBeLoaded(){
+
+    }
+
+    @Test
+    uncompletingTaskItemChangesStatus(){
+
+    }
+
+    @Test
+    uncompletingTaskItemFailsWithInvalidIndex(){
+        
+    }
+
 }
