ce6a970f09848b5868fa4c920f5ab8f22ecf1f3a
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-11 10:39:30 -0500

    Add taskitem constructor with appropriate get and set methods

--- collica_p4/src/TaskItem.java
+++ collica_p4/src/TaskItem.java
@@ -1,2 +1,36 @@
 public class TaskItem {
+    private String title;
+    private String description;
+    private String dueDate;
+
+
+    public TaskItem(String title, String description, String dueDate){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String getDueDate(){
+        return this.dueDate;
+    }
+    
+    public void setTitle(String newTitle){
+        this.title = newTitle;
+    }
+
+    public void setDescription(String newDescription){
+        this.description = newDescription;
+    }
+
+    public void setDueDate(String newDueDate){
+        this.dueDate = newDueDate;
+    }
 }
