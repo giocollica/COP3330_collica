2272bb3a14fb91d9e585e7852833338bf53d3988
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-11 12:16:33 -0500

    Add method to add items to tasklist

--- collica_p4/src/TaskList.java
+++ collica_p4/src/TaskList.java
@@ -1,9 +1,29 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TaskList {
     private ArrayList<TaskItem> TaskList = new ArrayList<>();
 
     public TaskList(ArrayList<TaskItem> TaskList){
         this.TaskList = TaskList;
+    }
+
+    public void addItem(){
+        Scanner input = new Scanner(System.in);
+        TaskItem newItem = new TaskItem("null", "null", "null", false);
+
+        System.out.print("Task title: ");
+        String newTitle = input.next();
+        newItem.setTitle(newTitle);
+
+        System.out.println("Task description: ");
+        String newDescription = input.next();
+        newItem.setDescription(newDescription);
+
+        System.out.println("Task due date (YYYY-MM-DD): ");
+        String newDueDate = input.next();
+        newItem.setDueDate(newDueDate);
+
+        this.TaskList.add(newItem);
     }
 }
