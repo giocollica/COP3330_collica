aa7b17271c9837fb5109e0420162270a48f43b27
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-11 10:32:59 -0500

    Add methods to print menus

--- collica_p4/src/App.java
+++ collica_p4/src/App.java
@@ -1,2 +1,29 @@
 public class App {
+
+    public static void main(String[] args){
+        printMainMenu();
+        printOperationMenu();
+    }
+
+    public static void printMainMenu(){
+        System.out.println("Main Menu");
+        System.out.println("_________\n");
+        System.out.println("1) create a new list");
+        System.out.println("2) load an existing list");
+        System.out.println("3) quit");
+    }
+
+    public static void printOperationMenu(){
+        System.out.println("List Operation Menu");
+        System.out.println("___________________\n");
+        System.out.println("1) view the list");
+        System.out.println("2) add an item");
+        System.out.println("3) edit an item");
+        System.out.println("4) remove an item");
+        System.out.println("5) mark an item as completed");
+        System.out.println("6) unmark an item as completed");
+        System.out.println("7) save the current list");
+        System.out.println("8) quit to the main menu");
+    }
+
 }
