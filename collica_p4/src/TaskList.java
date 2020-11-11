61a041efa1daeb8ce5e6301024bed4502f2080d6
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-11 11:56:01 -0500

    Add constructor for TaskList

--- collica_p4/src/TaskList.java
+++ collica_p4/src/TaskList.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class TaskList {
+    private ArrayList<TaskItem> TaskList = new ArrayList<>();
+
+    public TaskList(ArrayList<TaskItem> TaskList){
+        this.TaskList = TaskList;
+    }
 }
