48e3a8f0a880763770154b867beb852aaeb57544
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-16 11:21:54 -0500

    Add exception handling to add, remove, and edit methods

--- collica_p4/src/TaskList.java
+++ collica_p4/src/TaskList.java
@@ -9,76 +9,138 @@ public class TaskList {
     }
 
     public void addItem(){
-        Scanner input = new Scanner(System.in);
-        TaskItem newItem = new TaskItem("null", "null", 0000,
-                                            00, 00, false);
+        boolean continueLoop = true;
 
-        System.out.print("Task title: ");
-        String newTitle = input.nextLine();
-        newItem.setTitle(newTitle);
+        while(continueLoop){
+            try{
+                Scanner input = new Scanner(System.in);
+                TaskItem newItem = new TaskItem("null", "null", 0000,
+                        00, 00, false);
 
-        System.out.print("Task description: ");
-        String newDescription = input.nextLine();
-        newItem.setDescription(newDescription);
+                System.out.print("Task title: ");
+                String newTitle = input.nextLine();
+                newItem.setTitle(newTitle);
 
-        System.out.print("Task due date year (YYYY): ");
-        String newDueDateYear = input.next();
-        newItem.setDueDateYear(Integer.parseInt(newDueDateYear));
-        System.out.print("Task due date month (MM): ");
-        String newDueDateMonth = input.next();
-        newItem.setDueDateMonth(Integer.parseInt(newDueDateMonth));
-        System.out.print("Task due date day (DD): ");
-        String newDueDateDay = input.next();
-        newItem.setDueDateDay(Integer.parseInt(newDueDateDay));
+                System.out.print("Task description: ");
+                String newDescription = input.nextLine();
+                newItem.setDescription(newDescription);
 
-        this.taskItemList.add(newItem);
+                System.out.print("Task due date year (YYYY): ");
+                String newDueDateYear = input.next();
+                newItem.setDueDateYear(Integer.parseInt(newDueDateYear));
+                System.out.print("Task due date month (MM): ");
+                String newDueDateMonth = input.next();
+                newItem.setDueDateMonth(Integer.parseInt(newDueDateMonth));
+                System.out.print("Task due date day (DD): ");
+                String newDueDateDay = input.next();
+                newItem.setDueDateDay(Integer.parseInt(newDueDateDay));
+
+                this.taskItemList.add(newItem);
+
+                continueLoop = false;
+            }catch (IndexOutOfBoundsException indexOutOfBoundsException){
+                System.out.println("Index does not exist. Please try again.");
+            }
+        }
+
+
     }
 
     public void addItemHardCode(String title, String description, int dueDateYear, int dueDateMonth, int dueDateDay){
-        Scanner input = new Scanner(System.in);
-        TaskItem newItem = new TaskItem(title, description, dueDateYear,
-                dueDateMonth, dueDateDay, false);
+        boolean continueLoop = true;
 
-        this.taskItemList.add(newItem);
+        while(continueLoop){
+            try{
+                Scanner input = new Scanner(System.in);
+                TaskItem newItem = new TaskItem(title, description, dueDateYear,
+                        dueDateMonth, dueDateDay, false);
+
+                this.taskItemList.add(newItem);
+
+                continueLoop = false;
+            }catch (IndexOutOfBoundsException indexOutOfBoundsException){
+                System.out.println("Index does not exist. Please try again.");
+            }
+        }
+
+
+
     }
 
     public void removeItem(){
-        Scanner input = new Scanner(System.in);
+        boolean continueLoop = true;
 
-        System.out.print("Which task will you remove? ");
-        String taskToRemoveString = input.next();
-        int taskToRemove = Integer.parseInt(taskToRemoveString);
+        while(continueLoop){
+            try{
+                Scanner input = new Scanner(System.in);
 
-        this.taskItemList.remove(taskToRemove);
+                System.out.print("Which task will you remove? ");
+                String taskToRemoveString = input.next();
+                int taskToRemove = Integer.parseInt(taskToRemoveString);
+
+                this.taskItemList.remove(taskToRemove);
+
+                continueLoop = false;
+            }catch (IndexOutOfBoundsException indexOutOfBoundsException){
+                System.out.println("Index does not exist. Please try again.");
+            }
+        }
+
+
+
     }
 
     public void removeItemHardCode(int taskToRemove){
-        this.taskItemList.remove(taskToRemove);
+        boolean continueLoop = true;
+
+        while(continueLoop){
+            try{
+                this.taskItemList.remove(taskToRemove);
+
+                continueLoop = false;
+            } catch (IndexOutOfBoundsException indexOutOfBoundsException){
+                System.out.println("Index does not exist. Please try again.");
+            }
+        }
+
+
     }
 
     public void editItem(){
-        Scanner input = new Scanner(System.in);
+        boolean continueLoop = true;
 
-        System.out.println("Which task will you edit? ");
-        int taskToEdit = input.nextInt();
+        while(continueLoop){
+            try{
+                Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter a new title for task " + taskToEdit + ":");
-        String newTitle = input.nextLine();
-        this.taskItemList.get(taskToEdit).setTitle(newTitle);
+                System.out.println("Which task will you edit? ");
+                int taskToEdit = input.nextInt();
 
-        System.out.println("Enter a new description for task " + taskToEdit + ":");
-        String newDescription = input.nextLine();
-        this.taskItemList.get(taskToEdit).setDescription(newDescription);
+                System.out.println("Enter a new title for task " + taskToEdit + ":");
+                String newTitle = input.nextLine();
+                this.taskItemList.get(taskToEdit).setTitle(newTitle);
 
-        System.out.println("Enter a new due date year (YYYY) for task " + taskToEdit + ":");
-        int newDueDateYear = input.nextInt();
-        this.taskItemList.get(taskToEdit).setDueDateYear(newDueDateYear);
-        System.out.println("Enter a new due date month (MM) for task " + taskToEdit + ":");
-        int newDueDateMonth = input.nextInt();
-        this.taskItemList.get(taskToEdit).setDueDateMonth(newDueDateMonth);
-        System.out.println("Enter a new due date day (DD) for task " + taskToEdit + ":");
-        int newDueDateDay = input.nextInt();
-        this.taskItemList.get(taskToEdit).setDueDateDay(newDueDateDay);
+                System.out.println("Enter a new description for task " + taskToEdit + ":");
+                String newDescription = input.nextLine();
+                this.taskItemList.get(taskToEdit).setDescription(newDescription);
+
+                System.out.println("Enter a new due date year (YYYY) for task " + taskToEdit + ":");
+                int newDueDateYear = input.nextInt();
+                this.taskItemList.get(taskToEdit).setDueDateYear(newDueDateYear);
+                System.out.println("Enter a new due date month (MM) for task " + taskToEdit + ":");
+                int newDueDateMonth = input.nextInt();
+                this.taskItemList.get(taskToEdit).setDueDateMonth(newDueDateMonth);
+                System.out.println("Enter a new due date day (DD) for task " + taskToEdit + ":");
+                int newDueDateDay = input.nextInt();
+                this.taskItemList.get(taskToEdit).setDueDateDay(newDueDateDay);
+
+                continueLoop = false;
+            } catch (IndexOutOfBoundsException indexOutOfBoundsException){
+                System.out.println("Index does not exist. Please try again.");
+            }
+        }
+
+
     }
 
     public void markCompleted(){
@@ -153,3 +215,18 @@ public class TaskList {
         return this.taskItemList;
     }
 }
+
+
+
+    boolean continueLoop = true;
+
+        while(continueLoop){
+                try{
+
+
+                continueLoop = false;
+                }
+                }
+
+
+                System.out.println("Index does not exist. Please try again.");
