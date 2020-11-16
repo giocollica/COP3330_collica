7fa40eb69852242793e33ca64d7ce5365a233b26
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-15 21:09:54 -0500

    add new tests

--- collica_p4/src/TaskListTest.java
+++ collica_p4/src/TaskListTest.java
@@ -1,15 +1,33 @@
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TaskListTest {
     @Test
     public void addingTaskItemsIncreasesSize(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        int size = 0;
+
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        assertEquals(newTaskList.getTaskItemList().size(), size + 2);
     }
 
     @Test
     public void completingTaskItemChangesStatus(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+
+        newTaskList.markCompletedHardCode(0);
+
+        assertEquals(newTaskList.getTaskItemList().get(0).getComplete(), true);
     }
 
     @Test
@@ -19,12 +37,32 @@ public class TaskListTest {
 
     @Test
     public void editingTaskItemChangesValues(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        String newTitle = "This is not a test";
+
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+
+        newTaskList.getTaskItemList().get(0).setTitle("This is not a test");
+
+        assertEquals(newTaskList.getTaskItemList().get(0).getTitle(), newTitle);
     }
 
     @Test
     public void editingTaskItemDescriptionChangesValue(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        String newDescription = "This is not a test";
+
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+
+        newTaskList.getTaskItemList().get(0).setDescription("This is not a test");
+
+        assertEquals(newTaskList.getTaskItemList().get(0).getDescription(), newDescription);
     }
 
     @Test
@@ -34,7 +72,17 @@ public class TaskListTest {
 
     @Test
     public void editingTaskItemDueDateChangesValue(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        int newDueDateYear = 2021;
+
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+
+        newTaskList.getTaskItemList().get(0).setDueDateYear(2021);
+
+        assertEquals(newTaskList.getTaskItemList().get(0).getDueDateYear(), newDueDateYear);
     }
 
     @Test
@@ -44,7 +92,17 @@ public class TaskListTest {
 
     @Test
     public void editingTaskItemTitleChangesValue(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        String newTitle = "This is a test";
+
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+
+        newTaskList.getTaskItemList().get(0).setTitle("This is a test");
+
+        assertEquals(newTaskList.getTaskItemList().get(0).getTitle(), newTitle);
     }
 
     @Test
@@ -59,7 +117,15 @@ public class TaskListTest {
 
     @Test
     public void gettingTaskItemDescriptionSucceedsWithValidIndex(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        String expectedDescription = "Test";
+
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+
+        assertEquals(newTaskList.getTaskItemList().get(0).getDescription(), expectedDescription);
     }
 
     @Test
@@ -69,7 +135,15 @@ public class TaskListTest {
 
     @Test
     public void gettingTaskItemDueDateSucceedsWithValidIndex(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        int expectedDueDateYear = 2020;
+
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+
+        assertEquals(newTaskList.getTaskItemList().get(0).getDueDateYear(), expectedDueDateYear);
     }
 
     @Test
@@ -79,12 +153,23 @@ public class TaskListTest {
 
     @Test
     public void gettingTaskItemTitleSucceedsWithValidIndex(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        String expectedTitle = "Test";
+
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+        newTaskList.addItemHardCode("Test", "Test", 2020, 12, 25);
+
+        assertEquals(newTaskList.getTaskItemList().get(0).getTitle(), expectedTitle);
     }
 
     @Test
     public void newTaskListIsEmpty(){
+        ArrayList<TaskItem> taskItemList = new ArrayList<>();
+        TaskList newTaskList = new TaskList(taskItemList);
 
+        assertEquals(newTaskList.getTaskItemList().size(), 0);
     }
 
     @Test
