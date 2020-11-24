33403196ca792e5d2ad36d197bde7507333269e9
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-23 23:25:55 -0500

    Add prototype methods for tests

--- collica_p5/src/ContactItemTest.java
+++ collica_p5/src/ContactItemTest.java
@@ -1,2 +1,68 @@
+import org.junit.jupiter.api.Test;
+
 public class ContactItemTest {
+    @Test
+    public void creationFailsWithAllBlankValues(){
+
+    }
+
+    @Test
+    public void creationSucceedsWithBlankEmail(){
+
+    }
+
+    @Test
+    public void creationSucceedsWithBlankFirstName(){
+
+    }
+
+    @Test
+    public void creationSucceedsWithBlankLastName(){
+
+    }
+
+    @Test
+    public void creationSucceedsWithBlankPhone(){
+
+    }
+
+    @Test
+    public void creationSucceedsWithNonBlankValues(){
+
+    }
+
+    @Test
+    public void editingFailsWithAllBlankValues(){
+
+    }
+
+    @Test
+    public void editingSucceedsWithBlankEmail(){
+
+    }
+
+    @Test
+    public void editingSucceedsWithBlankFirstName(){
+
+    }
+
+    @Test
+    public void editingSucceedsWithBlankLastName(){
+
+    }
+
+    @Test
+    public void editingSucceedsWithBlankPhone(){
+
+    }
+
+    @Test
+    public void editingSucceedsWithNonBlankValues(){
+
+    }
+
+    @Test
+    public void testToString(){
+
+    }
 }
--- collica_p5/src/ContactListTest.java
+++ collica_p5/src/ContactListTest.java
@@ -1,2 +1,68 @@
+import org.junit.jupiter.api.Test;
+
 public class ContactListTest {
+    @Test
+    public void addingItemsIncreasesSize(){
+
+    }
+
+
+    @Test
+    public void editingItemsFailsWithAllBlankValues(){
+
+    }
+
+
+    @Test
+    public void editingItemsFailsWithInvalidIndex(){
+
+    }
+
+
+    @Test
+    public void editingSucceedsWithBlankFirstName(){
+
+    }
+
+
+    @Test
+    public void editingSucceedsWithBlankLastName(){
+
+    }
+
+
+    @Test
+    public void editingSucceedsWithBlankPhone(){
+
+    }
+
+
+    @Test
+    public void editingSucceedsWithNonBlankValues(){
+
+    }
+
+
+    @Test
+    public void newListIsEmpty(){
+
+    }
+
+
+    @Test
+    public void removingItemsDecreasesSize(){
+
+    }
+
+
+    @Test
+    public void removingItemsFailsWithInvalidIndex(){
+
+    }
+
+
+    @Test
+    public void savedContactListCanBeLoaded(){
+
+    }
 }
