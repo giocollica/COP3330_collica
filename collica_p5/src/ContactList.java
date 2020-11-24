ea96f753f7de3d7993d3694b78d0001a668ab3de
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-23 23:17:47 -0500

    Add constructor

--- collica_p5/src/ContactList.java
+++ collica_p5/src/ContactList.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class ContactList {
+    private ArrayList<ContactItem> contactList = new ArrayList<>();
+
+    public ContactList(ArrayList<ContactItem> contactList) {
+        this.contactList = contactList;
+    }
 }
