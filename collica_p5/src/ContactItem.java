15662c505f6d400c47c8dda7e765b04073c4ffe9
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-23 23:15:49 -0500

    Add constructor

--- collica_p5/src/ContactItem.java
+++ collica_p5/src/ContactItem.java
@@ -1,2 +1,13 @@
 public class ContactItem {
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String emailAddress;
+
+    public ContactItem(String firstName, String lastName, int phoneNumber, String emailAddress){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
 }
