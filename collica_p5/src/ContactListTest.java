6be6df31e739d915e61420cf5ac082f6d256b57c
Giovanni Collica <giovannijcollica@gmail.com>
2020-11-26 13:51:34 -0500

    add required tests

--- collica_p5/src/ContactListTest.java
+++ collica_p5/src/ContactListTest.java
@@ -1,9 +1,21 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ContactListTest {
     @Test
     public void addingItemsIncreasesSize(){
+        ArrayList<ContactItem> contactItemList = new ArrayList<>();
+        ContactList newContactList = new ContactList(contactItemList);
 
+        int size = 0;
+
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+        assertEquals(newContactList.getContactList().size(), size + 2);
     }
 
 
@@ -15,49 +27,110 @@ public class ContactListTest {
 
     @Test
     public void editingItemsFailsWithInvalidIndex(){
+        ArrayList<ContactItem> contactItemList = new ArrayList<>();
+        ContactList newContactList = new ContactList(contactItemList);
 
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newContactList.getContactList().get(2).setFirstName("This is a test"));
     }
 
 
     @Test
     public void editingSucceedsWithBlankFirstName(){
+        ArrayList<ContactItem> contactItemList = new ArrayList<>();
+        ContactList newContactList = new ContactList(contactItemList);
 
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+
+        newContactList.getContactList().get(0).setFirstName("");
+
+        assertEquals(newContactList.getContactList().get(0).getFirstName(), "");
     }
 
 
     @Test
     public void editingSucceedsWithBlankLastName(){
+        ArrayList<ContactItem> contactItemList = new ArrayList<>();
+        ContactList newContactList = new ContactList(contactItemList);
 
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+
+        newContactList.getContactList().get(0).setLastName("");
+
+        assertEquals(newContactList.getContactList().get(0).getLastName(), "");
     }
 
 
     @Test
     public void editingSucceedsWithBlankPhone(){
+        ArrayList<ContactItem> contactItemList = new ArrayList<>();
+        ContactList newContactList = new ContactList(contactItemList);
 
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+
+        newContactList.getContactList().get(0).setPhoneNumber(0);
+
+        assertEquals(newContactList.getContactList().get(0).getPhoneNumber(), 0);
     }
 
 
     @Test
     public void editingSucceedsWithNonBlankValues(){
+        ArrayList<ContactItem> contactItemList = new ArrayList<>();
+        ContactList newContactList = new ContactList(contactItemList);
 
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+
+        newContactList.getContactList().get(0).setFirstName("This is a test");
+        newContactList.getContactList().get(0).setLastName("This is a test");
+
+        assertEquals(newContactList.getContactList().get(0).getFirstName(), "This is a test");
+        assertEquals(newContactList.getContactList().get(0).getLastName(), "This is a test");
     }
 
 
     @Test
     public void newListIsEmpty(){
+        ArrayList<ContactItem> contactItemList = new ArrayList<>();
+        ContactList newContactList = new ContactList(contactItemList);
 
+        assertEquals(newContactList.getContactList().size(), 0);
     }
 
 
     @Test
     public void removingItemsDecreasesSize(){
+        ArrayList<ContactItem> contactItemList = new ArrayList<>();
+        ContactList newContactList = new ContactList(contactItemList);
 
+        int size = 0;
+
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+
+        newContactList.removeItemHardCode(0);
+
+        assertEquals(newContactList.getContactList().size(), size + 1);
     }
 
 
     @Test
     public void removingItemsFailsWithInvalidIndex(){
+        ArrayList<ContactItem> contactItemList = new ArrayList<>();
+        ContactList newContactList = new ContactList(contactItemList);
 
+        int size = 0;
+
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+        newContactList.addItemHardCode("Test", "Test", 2020, "Test");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newContactList.removeItemHardCode(2));
     }
 
 
